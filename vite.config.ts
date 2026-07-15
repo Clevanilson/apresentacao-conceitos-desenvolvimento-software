f0cc3c2424668b2a7e3ac7dd9059be7807f6f973
Clevanilson <clevanilson.contato@gmail.com>
2026-07-14 23:19:26 -0300

    build: adiciona workflow com build github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/apresentacao-conceitos-desenvolvimento-software/',
   plugins: [
     vue(),
     vueDevTools(),
